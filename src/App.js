01e066ca73a4692a1a8d9657b1f7e339d5df43ed
mukeshvijayakumar <mukesh@techasoft.com>
2025-01-02 14:52:33 +0530

    cra & tailwind

--- src/App.js
+++ src/App.js
@@ -3,11 +3,9 @@ import "./App.css";
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <p> hello world</p>
-      </header>
+      <h1 className="text-3xl font-bold underline">Hello world!</h1>
     </div>
   );
 }
 
-export default App;
+export default App; 
